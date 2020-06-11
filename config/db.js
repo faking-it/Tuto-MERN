17b35262f94e24fa4585b824301db7317399110b
faking-it <michelbruno453@hotmail.com>
2020-06-11 10:34:48 +0200

    Connexion à la DB + ajouts des routes/api

---
+++ config/db.js
@@ -0,0 +1,27 @@
+// Import des modules mongosse et config
+const mongoose = require("mongoose");
+const config = require("config");
+// On va chercher le lien de connexion à la BD dans le default.json
+const db = config.get("mongoURI");
+
+// C'est grâce à cette fonction que server.js va pouvoir se connecter à la BD
+// On utilise une asynchrone car plus propre niveau codage
+const connectDB = async () => {
+  try {
+    // Vu que connect() va retourner une promise, on place un await juste avant pour attendre son exécution
+    // Le premier paramètre que reçoit connect() est le lien de connexion à la DB
+    // Le second paramètre est un array avec toutes les options de connexion
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.log(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
+// On doit d'abord importer le module Express
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+// Connexion à la Base de Données
+connectDB();
+
+// La fonction req.send envoie des données au navigateur, ici ("API Running")
 app.get("/", (req, res) => res.send("API Running"));
 
+// process.env.PORT est le port qui sera généré une fois le projet hébergé sur Heroku
+// Pour l'instant, vu qu'on travaille en local, on définit le port 5000.
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
