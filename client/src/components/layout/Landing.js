c0e3694565e5ad930c7d32613e90e361e22855a4
faking-it <michelbruno453@hotmail.com>
2020-06-17 12:08:30 +0200

    Auth completed

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -6,11 +6,8 @@ export const Landing = () => {
     <section className="landing">
       <div className="dark-overlay">
         <div className="landing-inner">
-          <h1 className="x-large">Developer Connector</h1>
-          <p className="lead">
-            Create developer profile/portfolio, share posts and get help from
-            other developers
-          </p>
+          <h1 className="x-large">Mwenbwa</h1>
+          <p className="lead">Add some description</p>
           <div className="buttons">
             <Link to="/register" className="btn btn-primary">
               Sign Up
