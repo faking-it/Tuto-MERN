139f7066648ad4a860ce0dc78b5779f8cc66473a
faking-it <michelbruno453@hotmail.com>
2020-07-02 11:24:29 +0200

    null tree value after 15min bug fixed - Bruno

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,11 +5,9 @@ import PropTypes from "prop-types";
 import { logout } from "../../actions/auth";
 import Clock from "react-live-clock";
 
-
 const Navbar = ({ auth: { isAuthenticated, loading, user }, logout }) => {
   const authLinks = (
     <ul>
-
       <li>
         <div>
           <i className="fa fa-tree" aria-hidden="true" />{" "}
